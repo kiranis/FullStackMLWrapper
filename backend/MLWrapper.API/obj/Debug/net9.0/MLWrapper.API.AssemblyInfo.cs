10c88708a437f64a8b663875b4c38473ba27a64f
kiranis <anis.touati@gmail.com>
2025-07-14 22:45:01 -0400

    adding git ignore file and adjusting .net version

--- backend/MLWrapper.API/obj/Debug/net9.0/MLWrapper.API.AssemblyInfo.cs
+++ backend/MLWrapper.API/obj/Debug/net9.0/MLWrapper.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MLWrapper.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+293ab4ecb9f57d16c27eadc6003a71ad1bad4b59")]
 [assembly: System.Reflection.AssemblyProductAttribute("MLWrapper.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MLWrapper.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
